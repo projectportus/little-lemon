28def301392932c2f9cf3d8504c0ccb7cb23a19c
RyanGosling698 <iskander102005@gmail.com>
2025-04-02 23:36:14 +0500

    some changes goes here

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+        <div className="logo">
+    
+        </div>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Menu</a>
+            <a href="#">Reservations</a>
+            <a href="#">Order Online</a>
+            <a href="#">Login</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
